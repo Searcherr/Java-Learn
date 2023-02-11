1320dd1f1e5c83089af5449d631cc8979b8c76c4
Searcherr <alfafemida@gmail.com>
2023-02-11 23:09:19 +0100

    Started writing Clock Class

--- src/main/java/org/example/Clock.java
+++ src/main/java/org/example/Clock.java
@@ -1,13 +1,23 @@
 package org.example;
 
 import lombok.Data;
+import lombok.NonNull;
+
+import java.lang.reflect.Array;
+import java.util.List;
+
 // 55. Write a Java program to convert seconds to hour, minute and seconds.
 @Data
 public class Clock {
+    @NonNull
     private int seconds;
-    private int minutes;
-    private int hours;
+    private int minutes = 0;
+    private int hours = 0;
 
+    public List<Integer> getTime(){
+        int[] time = new int[3];
+
+    }
 
 
 }
